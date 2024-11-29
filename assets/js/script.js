13fd805b45dee1607966314df23e45febc7ee016
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2024-11-29 18:09:54 +0100

    start 576

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,16 @@ const header = document.querySelector("header");
 const navBtn = document.querySelector("nav ul li button")
 document.addEventListener("scroll", function () {
 
-    if (document.documentElement.scrollTop > 450) {
+    if (document.documentElement.scrollTop > 500) {
         header.classList.add("headerColor2");
-        header.classList.remove("headerColor1");
+        
+
         navBtn.classList.add("btnColor2");
         navBtn.classList.remove("btnColor1");
     } else {
         header.classList.remove("headerColor2");
         header.classList.add("headerColor1");
+
         navBtn.classList.remove("btnColor2");
         navBtn.classList.add("btnColor1")
     }
